f5334a9f75d5eb329569633745b53cebcd2008be
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-11-06 03:00:38 -0500

    ajuste

--- src/app/login/autenticacion.service.ts
+++ src/app/login/autenticacion.service.ts
@@ -5,6 +5,8 @@ import { InformacionAutorizacion } from './informacion-autorizacion';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,8 +21,10 @@ export class AutenticacionService {
     return this.http.post<InformacionAutorizacion>(this.autenticarseUrl, JSON.stringify(autenticacion), httpOptions);
   }
 
-  login(correo: string, password: string) {
-    return this.auth.signInWithEmailAndPassword(correo, password);
+  async login(correo: string, password: string) {
+    return await signInWithEmailAndPassword(getAuth(), correo, password)
+
+    //return this.auth.signInWithEmailAndPassword(correo, password);
   }
 
 
